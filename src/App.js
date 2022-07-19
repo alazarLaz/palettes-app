1add8f049f1fccee9777423d8bf2e3e636e4285a
alazarLaz <alazar.yeh@gmail.com>
2022-07-20 02:24:48 +0300

    Color Helper Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import generatePalette from './colorHelper';
 import colorSeed from './colorSeed'
 import Palette from './Palette';
 
 function App() {
+  console.log(generatePalette(colorSeed[4]))
   return (
     <div className="App">
       <Palette {...colorSeed[4]} />
