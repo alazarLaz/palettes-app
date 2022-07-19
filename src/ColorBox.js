eddd52a3a8a7a4e42d1610ab77efa8f4bbeff29d
alazarLaz <alazar.yeh@gmail.com>
2022-07-20 00:43:30 +0300

    Styled ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react'
 import './ColorBox.css'
+import CopyToClipboard from 'react-copy-to-clipboard'
 
 export default class ColorBox extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        copied:false
+      }
+      this.copyColor = this.copyColor.bind(this)
+      this.handleClick = this.handleClick.bind(this)
+    }
+    
+    handleClick(evt)
+    {
+        this.copyColor()
+    }
+    copyColor(){
+        // Clipboard.apply
+        this.setState({copied:true}
+            ,()=>setTimeout(()=>{
+            this.setState({
+                copied:false
+            })
+        },1500)
+        )
+    }
+
+
   render() {
+    const { copied } = this.state;
     return (
-      <div style={{backgroundColor: `${this.props.color}`}} className='ColorBox'>
-        <div className='copy-container'>
-        <div className='box-content'>
-            <span>{this.props.title}</span>
-        </div>
-        <button className='copy-button'>Copy</button>        
-      </div>
-      <div className='see-more'>More</div>
-    </div>
+        <CopyToClipboard onCopy = {this.copyColor} text = {this.props.color}>
+            <div style={{background: `${this.props.color}`}} className='ColorBox'>
+                <div
+                    style={{background: `${this.props.color}`}}
+                    className={`copy-overlay ${copied?' show':''}`}
+                 />
+                <div className={`copied ${copied?' show':''}`}>
+                        <h1>Copied!</h1>
+                        
+                        <p>{this.props.color}</p>
+                    </div>
+
+                <div className='copy-container'>
+                    <div className='box-content'>
+                        <span>{this.props.title}</span>
+                    </div>
+                    <button onClick={this.handleClick} className='copy-button'>Copy</button>        
+                </div>
+                <span className='see-more'>More</span>
+            </div>
+    </CopyToClipboard>
+
     )
   }
 }
+{/* <div 
+                style={{background: `${this.props.color}`}}
+                 className={`copy-overlay ${copied?' show':''}`}
+                 /> */}
--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox'
 import './Pallette.css'
+import { v4 as uuid } from 'uuid';
 
 export default class Palette extends Component {
   render() {
-    let colorBox = this.props.colors.map(color => <ColorBox color = {color.color} title = {color.name} />)
+    let colorBox = this.props.colors.map(color => <ColorBox key={uuid()} color = {color.color} title = {color.name} />)
     return (
       <div className='Palette'>
         {/* NavBar */}
